cc21bcce4692512ccf32347f11f06bed0c7ce1d2
AndreyLapko1 <andreycgo765@gmail.com>
2025-02-25 21:07:29 +0300

    add new endpoints to aggregate tasks

--- task_manager_app/serializer.py
+++ task_manager_app/serializer.py
@@ -3,7 +3,19 @@ from .models import Task
 from .models import SubTask
 
 
+class SubTaskSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = SubTask
+        fields = '__all__'
+
+
 class TaskSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Task
+        fields = '__all__'
+
+
+class CreateTaskSerializer(serializers.ModelSerializer):
     class Meta:
         model = Task
         fields = ['title', 'description', 'status', 'deadline']
--- task_manager_app/urls.py
+++ task_manager_app/urls.py
@@ -3,4 +3,9 @@ from . import views
 
 urlpatterns = [
     path('task_create/', views.task_create, name='task_create'),
+    path('task_list/', views.get_task_list, name='task_list'),
+    path('task_detail/<int:pk>', views.get_task_detail, name='task_detail'),
+    path('task_count/', views.get_task_count, name='get_task_count'),
+    path('status/<str:status>', views.count_task_by_status, name='count_task_by_status'),
+    path('overdue_task/', views.count_overdue_tasks, name='count_overdue_tasks'),
 ]
