f94bad2631582d0501ed8482a107a6d8e199bde7
AndreyLapko1 <andreycgo765@gmail.com>
2025-03-02 13:15:10 +0300

    add class views

--- task_manager_app/urls.py
+++ task_manager_app/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     path('task_list/', views.TaskListCreateView.as_view(), name='task_list'),
     path('create_subtask/', views.SubTaskListCreateView.as_view(), name='create_subtask'),
     path('update_subtask/<int:pk>', views.SubTaskDetailUpdateDeleteView.as_view(), name='update_delete_subtask'),
-    path('task_count/', views.get_task_count, name='get_task_count'),
-    path('status/<str:status>', views.count_task_by_status, name='count_task_by_status'),
-    path('overdue_task/', views.count_overdue_tasks, name='count_overdue_tasks'),
+    path('task_count/', views.TaskCountView.as_view(), name='get_task_count'),
+    path('status/<str:stat>', views.TaskCountView.as_view(), name='count_task_by_status'),
+    path('overdue_task/', views.TaskOverdueCountView.as_view(), name='count_overdue_tasks'),
 ]
--- task_manager_app/views.py
+++ task_manager_app/views.py
@@ -149,35 +149,34 @@ class TaskDetailCreateUpdateDeleteView(APIView):
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
+class TaskCountView(APIView):
+
+    def get(self, request, stat=None):
+        if stat is None:
+            tasks = Task.objects.all().count()
+            return Response({'Count tasks': tasks}, status=status.HTTP_200_OK)
+        if stat:
+            valid_statuses = ['new', 'in_progress', 'pending', 'blocked', 'done']
+            if stat not in valid_statuses:
+                return Response({'message': 'Status not supported'})
+            tasks = Task.objects.filter(status=stat)
+            if not tasks:
+                return Response({'message': 'Task not found'}, status=status.HTTP_404_NOT_FOUND)
+            serializer = TaskSerializer(tasks, many=True)
+            return Response(serializer.data)
+
+class TaskOverdueCountView(APIView):
+    def get(self, request):
+        count = 0
+        overdue_task_list = []
+        overdue_tasks = Task.objects.all()
+        for task in overdue_tasks:
+            if task.deadline < timezone.now():
+                overdue_task_list.append(task.title)
+                count += 1
+        return Response({'Count overdue task': count, 'Overdue tasks': f'{overdue_task_list}'}, status=status.HTTP_200_OK)
 
 
-
-@api_view(['GET'])
-def get_task_count(request):
-    tasks = Task.objects.all().count()
-    return Response({'Count tasks': tasks}, status=status.HTTP_200_OK)
-
-@api_view(['GET'])
-def count_task_by_status(request, status):
-    valid_statuses = ['new', 'in_progress', 'pending', 'blocked', 'done']
-    if status not in valid_statuses:
-        return Response({'message': 'Status not supported'})
-    tasks = Task.objects.filter(status=status)
-    if not tasks:
-        return Response({'message': 'Task not found'}, status=status.HTTP_404_NOT_FOUND)
-    serializer = TaskSerializer(tasks, many=True)
-    return Response(serializer.data)
-
-@api_view(['GET'])
-def count_overdue_tasks(request):
-    count = 0
-    overdue_task_list = []
-    overdue_tasks = Task.objects.all()
-    for task in overdue_tasks:
-        if task.deadline < timezone.now():
-            overdue_task_list.append(task.title)
-            count += 1
-    return Response({'Count overdue task': count, 'Overdue tasks': f'{overdue_task_list}'}, status=status.HTTP_200_OK)
 
 
 # Create your views here.
