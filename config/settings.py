efda3a3f826f6f1b7cb76e5045e1e461a4e4fe2e
AndreyLapko1 <andreycgo765@gmail.com>
2025-03-25 21:01:33 +0300

    fixed pagination -> page_size

--- config/settings.py
+++ config/settings.py
@@ -134,7 +134,7 @@ SIMPLE_JWT = {
 
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 6 ,
+    'PAGE_SIZE': 5,
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     )
